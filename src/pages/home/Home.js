13cf00f9e9dc0e58e217071c8c923c2eb45ba3f6
shravan meena <shravansymita@shravans-MacBook-Pro.local>
2024-05-18 00:16:57 +0530

    working code here

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -401,7 +401,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="px-4 md:p-20">
+    <div className="px-4 md:px-20">
       <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
     </div>
   );
